129115014264d87136f03719167d118e18ebdb96
Matti Schulze <matti.schulze@fau.de>
2021-05-09 13:47:39 +0200

    - added documentation - removed old send_mdh_request.py

--- src/mdh_bridge.py
+++ src/mdh_bridge.py
@@ -9,6 +9,41 @@ Objects in query arguments are not supported so fat :(
 
 """
 
+"""
+This class allows for easy communication with the Metadatahub webql. For this it offers classes, mirroring 
+every query or object that can be used in the MDH. All of these classes inherit MDHObject, which offers 
+functionality to (de-)serialize the contents from the class into a query, or the result of a query into the classes.
+All Queries inherit from MDHQuery, which implements a different serialization algorithm.
+
+The usage of this functionality can be broken down into a few steps:
+1. Set up a MDHQueryRoot class. This class contains a list of queries that will be executed in the MDH. 
+    query_root = MDHQueryRoot()
+
+2. Set up an MDHQuery. For this you have to create some new Query, like this:
+    query = MDHQuery_searchMetadata()
+Every query has some flags as their members, which are default set to False. When they are set to another value, that
+value is used as an argument for their query. For example, when you only want to search metadata with a certain fileId:
+    query.fileIds = [1, 2, 3, 4, 5]
+    
+3. Set the result field of the Query: For every query, the result field has to be specified. This field is repsonsible,
+for telling the MDH which of the resulting objects attributes you actually want to get returned. For example, if you 
+only want to see the total number of results and all the metadata for all the resulting classes, use the following:
+    query.result.totalFilesCount = True
+    query.result.files = MDHFile()
+    query.result.files.metadata = MDHMetadata()
+    query.result.files.metadata.metadatum.name = True
+    query.result.files.metadata.metadatum.value = True
+    
+4. Add the query to the query_root and execute it:
+    query_root.queries.append(query)
+    query_root.build_and_send_request()    
+    
+5. Access the queries results:
+    total_file_count = query_root.queries[0].result.totalFilesCount
+    ...
+
+"""
+
 MetadataOption = Enum("MetadataOption",
                       "NOT_CONTAINS EQUAL NOT_EQUAL GREATER SMALLER EXISTS NOT_EXISTS EMPTY NOT_EMPTY")
 

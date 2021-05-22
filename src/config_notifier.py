76ed500cff1485161a1992e54671a77d227cd274
Marlon Weghorn <marlon.weghorn@fau.de>
2021-05-22 12:56:42 +0200

    Replaced toml config with graphql config, adapted the notifier correspondingly, cleanup unnecessary methods etc.

---
+++ src/config_notifier.py
@@ -0,0 +1,45 @@
+# 3rd party imports
+import pyinotify
+from mdh_bridge import MDHQueryRoot
+
+# Local import
+from fuse_utils import build_tree_from_files
+
+
+class ConfigfileEventHandler(pyinotify.ProcessEvent):
+    """
+    Event handler that gets triggered when the "config.cfg" file changes. If this happens, this class is
+    responsible for updating the directory tree, according to the new filters
+    """
+
+    def __init__(self, fuse, core_name: str, config_path: str, **kargs):
+        """
+        Constructor for the class. For more information see pyinotify.ProcessEvent.__init()__
+        :param fuse: mdh_fuse for which the directory tree will be updated
+        :param kargs: optional arguments for the pyinotify.ProcessEvent constructor
+        """
+        super().__init__(**kargs)
+        self.config_path = config_path
+        self.core_name = core_name
+        self.fuse = fuse
+
+    def update_tree(self):
+        """
+        Update the directory tree in the fuse, according to the new config
+        :return: Nothing
+        """
+
+        print("Updating the directory tree")
+        query_root = MDHQueryRoot(self.core_name, self.config_path)
+        query_root.send_request()
+
+        self.fuse.metadatahub_files = query_root.result['searchMetadata']['files']
+        self.fuse.directory_tree = build_tree_from_files(self.metadatahub_files)
+
+    def process_IN_MODIFY(self, event):
+        """
+        gets called when a IN_MODIFY event is triggered on the config file
+        :param event: see parent class documentation; unused
+        :return: Nothing
+        """
+        self.update_tree()

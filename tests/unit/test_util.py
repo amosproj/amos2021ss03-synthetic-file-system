e1388310947742376f4e4446c45fb69f4ca63c3b
DOME_ <dominikscho@outlook.com>
2021-05-24 02:23:38 +0200

    skip comparsion test for for refactored version

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -10,7 +10,7 @@ from anytree import Node, Resolver, RenderTree
 from anytree.exporter import DictExporter
 
 # Local imports
-from src.fuse_utils import build_tree_from_files, build_tree_from_files_
+from src.fuse_utils import build_tree_from_files
 
 
 class TestUtils(unittest.TestCase):
@@ -21,20 +21,17 @@ class TestUtils(unittest.TestCase):
 
     def test_empty_tree_build(self) -> None:
         tree = build_tree_from_files([])
-        tree_new = build_tree_from_files_new([])
         d_tree = self.exporter.export(tree)
-        d_tree_new = self.exporter.export(tree_new)
-        self.assertCountEqual(d_tree, d_tree_new)
+        d_root_node = self.exporter.export(Node("Root"))
+        self.assertCountEqual(d_tree, d_root_node)
 
+    @unittest.skip("tested the new refactored version against the old on")
     def test_simple_build_tree(self) -> None:
         with open('./tests/unit/test_data.json', 'r') as f:
             test_object_list = json.load(f)
-
         for test_obj in test_object_list:
             tree = build_tree_from_files(test_obj)
             tree_new = build_tree_from_files_new(test_obj)
-            print(RenderTree(tree_new))
             d_tree = self.exporter.export(tree)
             d_tree_new = self.exporter.export(tree_new)
-
             self.assertCountEqual(d_tree, d_tree_new)

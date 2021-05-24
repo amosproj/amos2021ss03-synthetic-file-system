2efb74e2c57741ee835023e9f61c08060a23e0e0
DOME_ <dominikscho@outlook.com>
2021-05-24 02:21:06 +0200

    deleted old buil_tree version

--- src/fuse_utils.py
+++ src/fuse_utils.py
@@ -3,55 +3,6 @@ from anytree import Node, Resolver
 from typing import Dict, List
 
 
-def build_tree_from_files_(files: dict):
-    """
-    build_tree_from_files Turns a list of FilePaths into a directory tree. The tree is built upon a "Root" node
-    :param files: a list of file paths e.g ["/metadatahub/crawler/test", "/metadatahub/docs/test", ...]
-    :return: The root Node (anytree.Node) of the resulting tree
-    """
-
-    root_node = Node("Root")
-
-    # prepare directory names for easier processing
-    file_paths = []
-    for file in files:
-        full_file_name = ""
-        for metadata in file['metadata']:
-            if metadata['name'] == "SourceFile":
-                full_file_name = metadata['value']
-
-        file_paths.append(full_file_name.split("/"))
-
-    i = 1
-    num_files_to_parse = len(file_paths)
-    while True:
-        for file_path in file_paths:
-            if i >= len(file_path):
-                num_files_to_parse -= 1
-                continue
-            path_part = file_path[i]
-            prev_path = ""
-            for j in range(1, i):
-                prev_path += file_path[j] + "/"
-
-            prev_path = prev_path[:len(prev_path) - 1]  # remove trailing "/"
-
-            has_child = False
-            parent_finder = Resolver("name")
-            parent_node: Node
-            parent_node = parent_finder.get(root_node, prev_path)
-            for child in parent_node.children:
-                if child.name == path_part:
-                    has_child = True
-            if not has_child:
-                Node(path_part, parent_node)
-        if num_files_to_parse <= 0:
-            break
-        i += 1
-
-    return root_node
-
-
 def build_tree_from_files(files: List[Dict]) -> Node:
     """Builds a directory tree out of the results from the metadata-hub query.
     The tree is build upon a "Root" node which is returned at the end.
@@ -68,7 +19,6 @@ def build_tree_from_files(files: List[Dict]) -> Node:
     parent_finder = Resolver("name")
     file_paths = _extract_file_paths_parts(files)
     max_index = _length_of_longest_path(file_paths)
-    #print(file_paths)
     for i in range(max_index):
         # In every iteration of the outer loop we only work on parts up to position i
         for file_path in file_paths:
@@ -99,7 +49,6 @@ def _extract_file_paths_parts(files: List[Dict]) -> List[List[str]]:
             if metadata['name'] == "SourceFile":
                 full_file_path = metadata['value']
         file_paths.append(full_file_path)
-    #print(file_paths)
 
     file_paths_parts = []
     for file_path in file_paths:

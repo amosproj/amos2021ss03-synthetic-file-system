df53cbfbff1d7e3ad464e32cec19d515b821cdb6
Marlon Weghorn <marlon.weghorn@fau.de>
2021-05-25 17:54:31 +0200

    Removed unused imports

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -1,12 +1,11 @@
 """Unit tests: Tests the utility functions"""
 
 # Python imports
-import os
 import json
 import unittest
 
 # 3rd party imports
-from anytree import Node, Resolver, RenderTree
+from anytree import Node
 from anytree.exporter import DictExporter
 
 # Local imports
@@ -31,7 +30,7 @@ class TestUtils(unittest.TestCase):
             test_object_list = json.load(f)
         for test_obj in test_object_list:
             tree = build_tree_from_files(test_obj)
-            tree_new = build_tree_from_files_new(test_obj)
+            tree_new = build_tree_from_files(test_obj)
             d_tree = self.exporter.export(tree)
             d_tree_new = self.exporter.export(tree_new)
             self.assertCountEqual(d_tree, d_tree_new)

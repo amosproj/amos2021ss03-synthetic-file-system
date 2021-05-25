54f4bda13f927422058a7bb24abd08bfb7c0fc23
DOME_ <dominikscho@outlook.com>
2021-05-25 21:50:02 +0200

    rework test for build_tree and test_data

--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -24,13 +24,13 @@ class TestUtils(unittest.TestCase):
         d_root_node = self.exporter.export(Node("Root"))
         self.assertCountEqual(d_tree, d_root_node)
 
-    @unittest.skip("tested the new refactored version against the old on")
     def test_simple_build_tree(self) -> None:
         with open('./tests/unit/test_data.json', 'r') as f:
             test_object_list = json.load(f)
-        for test_obj in test_object_list:
-            tree = build_tree_from_files(test_obj)
-            tree_new = build_tree_from_files(test_obj)
-            d_tree = self.exporter.export(tree)
-            d_tree_new = self.exporter.export(tree_new)
-            self.assertCountEqual(d_tree, d_tree_new)
+        test_obj = test_object_list['test_obj1']
+        input = test_obj[0]
+        expected_output = test_obj[1]
+
+        tree = build_tree_from_files(input)
+        actual = self.exporter.export(tree)
+        self.assertCountEqual(actual, expected_output)

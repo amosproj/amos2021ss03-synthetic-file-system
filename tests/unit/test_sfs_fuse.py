b5dbb6bebf49738213d36a05fbcc0145253753e7
Marlon Weghorn <marlon.weghorn@fau.de>
2021-05-27 17:06:16 +0200

    Test skeleton for read calls

---
+++ tests/unit/test_sfs_fuse.py
@@ -0,0 +1,31 @@
+# Python imports
+import unittest
+
+
+class TestSFSFuse(unittest.TestCase):
+
+    # =======================
+    # Test Filesystem methods
+    # =======================
+
+    def test_access(self) -> None:
+        self.assertTrue(True)
+
+    def test_gettattr(self) -> None:
+        self.assertTrue(True)
+
+    def test_readdir(self) -> None:
+        self.assertTrue(True)
+
+    def test_readlink(self) -> None:
+        self.assertTrue(True)
+
+    def test_statfs(self) -> None:
+        self.assertTrue(True)
+
+    # =================
+    # Test File methods
+    # =================
+
+    def test_read(self) -> None:
+        self.assertTrue(True)

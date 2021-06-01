a3bd18ceddc15d78480136d366209b613f5aa9a7
sfsuser <sfsuser@vmdeb10-sfs-2.graudatastorage.intern>
2021-06-01 22:43:06 +0200

    Modified test_integ.py as per Marlon's comments.

--- tests/integration/test_integ.py
+++ tests/integration/test_integ.py
@@ -6,49 +6,42 @@ import time
 import sys
 import logging
 
-# Create logger
-logger = logging.getLogger()
-fhandler = logging.FileHandler(filename='/tmp/test_integ.log', mode='a')
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fhandler.setFormatter(formatter)
-logger.addHandler(fhandler)
-logger.setLevel(logging.DEBUG)
-
-
 # I am not sure how exactly are these integ tests written and the rules of calling tests within tests, hence just declared a global variable
-mounted_files = "global"
+#mounted_files = "global"
 # CHANGE_PATH
-fuse_mounted_files_list = "/tmp/Mounted_files.lst"
+#fuse_mounted_files_list = "/tmp/Mounted_files.lst"
 # CHANGE_NAME
-mdh_core = "core-sfs"
+#mdh_core = "core-sfs"
 # hardcoded the path for mounted_from for now. Can be later modified to be read from the json config file, or maybe an environment variable
 # CHANGE_PATH
-mounted_from="/home/sfsuser/Vaidehi/amos-rep/test_tree"
+#mounted_from="/home/sfsuser/Vaidehi/amos-rep/test_tree"
 
 # I don't understand how to work with this "self" in python.
+
+MOUNTED_FILES = ""
+FUSE_MOUNTED_FILES_LIST = "/tmp/Mounted_files.lst"
+MDH_CORE = "core-sfs"
+MOUNTED_FROM = "/home/sfsuser/Vaidehi/amos-rep/test_tree"
+
 
 
 class TestIntegratedTool(unittest.TestCase):
 
-    # Setup docker
     def setUp(self):
         print("/************************************* Entered setUp *************************************/")
+
         # Add docker stop $(docker ps -aq), docker rm $(docker ps -aq) -- may not be required now. Still keeping in comments if we have problems later and this is a resolution
         if not os.path.isfile('docker-compose.yml'):
             self.assertRaises(FileNotFoundError)
 
         # Check if mdh core is running
         is_core_up = subprocess.Popen(["docker", "container", "inspect", "-f",
-	                              "'{{.State.Running}}'", mdh_core], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	                              "'{{.State.Running}}'", MDH_CORE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = is_core_up.communicate()
         if "true" not in out.decode():
             self.assertFalse("MDH CORE NOT RUNNING!!!")
 
         docker_compose_up=subprocess.Popen(["docker-compose", "up"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # docker_up_out, docker_up_err = docker_compose_up.communicate()
-        # if docker_up_out.decode().find("error") == -1:
-            # self.assertFalse("DOCKER-COMPOSE UP COULD NOT BE EXECUTED!!!")
 
         # Check if subprocess is running
         is_up=docker_compose_up.poll()
@@ -65,27 +58,21 @@ class TestIntegratedTool(unittest.TestCase):
             # Added sleep because docker takes time to be set up
             time.sleep(30)
 
-        print("/************************************* Exiting setUp *************************************/")
     
     @pytest.mark.dependency()
     def test_mount(self):
         print("/************************************* Entered test_mount *************************************/")
 
-        # Could be removed. More for my VM right now.
+        # Start docker just in case it is down
         docker_start=subprocess.Popen(
             ["docker", "container", "start", "synthetic-file-system"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        docker_start_out, docker_start_err=docker_start.communicate()
-        docker_start_out = docker_start_out.decode()
-        docker_start_err = docker_start_err.decode()
 
         # Run mount.sh
-        # docker_mount = subprocess.Popen(["docker", "container", "exec", "-d", "synthetic-file-system", "./mount.sh"], capture_output=True)
         docker_mount=subprocess.Popen(["docker", "container", "exec", "-d", "synthetic-file-system",
                                          "./mount.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         docker_mount_out, docker_mount_err=docker_mount.communicate()
-        docker_mount_out = docker_mount_out.decode()
         docker_mount_err = docker_mount_err.decode()
         if "failed" in docker_mount_err:
              self.assertFalse("ERROR RUNNING MOUNT.SH!!!")
@@ -93,28 +80,27 @@ class TestIntegratedTool(unittest.TestCase):
         time.sleep(5)
 
         # goes into the container and gets a list of all files under fuse_mount and writes it to a file
-        mounted_files=os.popen(
+        MOUNTED_FILES=os.popen(
             'docker container exec synthetic-file-system /bin/sh -c "cd ..; cd /fuse_mount; find . -type f" ').read()
-        f=open(fuse_mounted_files_list, 'w')
-        print(mounted_files, file=f)
+        f=open(FUSE_MOUNTED_FILES_LIST, 'w')
+        print(MOUNTED_FILES, file=f)
         f.close()
 
         # Throws error if file containing mounted data is empty
-        if os.stat(fuse_mounted_files_list).st_size == 0:
+        if os.stat(FUSE_MOUNTED_FILES_LIST).st_size == 0:
             self.assertFalse("Mounted files list at " +
-                             fuse_mounted_files_list, " empty!!!")
+                             FUSE_MOUNTED_FILES_LIST, " empty!!!")
 
-        print("/************************************* Exiting test_mount *************************************/")
 
 
     @pytest.mark.dependency(depends=['test_mount'])
     def test_dir_structure(self):
         print("/************************************* Entered test_dir_structure *************************************/")
 
-        common_dir=mounted_from.split("/")
+        common_dir = MOUNTED_FROM.split("/")
 
         # Read all fuse files into an array
-        fuse_mounted_files=open(fuse_mounted_files_list, "r")
+        fuse_mounted_files=open(FUSE_MOUNTED_FILES_LIST, "r")
         fuse_abs_mount_lines=fuse_mounted_files.readlines()
         fuse_mounted_files.close()
 
@@ -123,7 +109,7 @@ class TestIntegratedTool(unittest.TestCase):
                             for i in fuse_abs_mount_lines]
 
         # Checks if files found at mounted_from were actually mounted on the vfs
-        for root, dirs, files in os.walk(mounted_from):
+        for root, dirs, files in os.walk(MOUNTED_FROM):
             for f in files:
                 # /home/sfsuser/Vaidehi/amos-rep/test_tree/dir0/dir144/dir158/dir160/dir164/dir165/CNV-1699976-2.jpeg
                 file_path=os.path.join(root, f)
@@ -132,7 +118,6 @@ class TestIntegratedTool(unittest.TestCase):
                 if (req_path[1] in fuse_mount_lines) == False:
                     self.assertRaises(FileNotFoundError)
 
-        print("/************************************* Exiting test_dir_structure *************************************/")
 
     # Turn off docker services
     def tearDown(self):
@@ -148,5 +133,3 @@ class TestIntegratedTool(unittest.TestCase):
         if is_down is None:
             self.assertFalse(
                 "************** COULD NOT SHUT DOWN DOCKER **************")
-
-        print("/************************************* Exiting tearDown *************************************/")

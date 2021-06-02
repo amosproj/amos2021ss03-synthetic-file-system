92e66955b7e9cc23579a66f43768e16dcfffb3f3
sfsuser <sfsuser@vmdeb10-sfs-2.graudatastorage.intern>
2021-06-02 15:12:42 +0200

    Finalized changed for integ test for creation of merge request.

--- tests/integration/test_integ.py
+++ tests/integration/test_integ.py
@@ -1,3 +1,7 @@
+# HOW TO EXECUTE?
+# First install the package: pip install pytest-docker-compose
+# Run with: pytest --docker-compose="path_to_docker_compose_yml" test_integ.py
+
 import pytest
 import os
 import subprocess
@@ -6,42 +10,32 @@ import time
 import sys
 import logging
 
-# I am not sure how exactly are these integ tests written and the rules of calling tests within tests, hence just declared a global variable
-#mounted_files = "global"
-# CHANGE_PATH
-#fuse_mounted_files_list = "/tmp/Mounted_files.lst"
-# CHANGE_NAME
-#mdh_core = "core-sfs"
-# hardcoded the path for mounted_from for now. Can be later modified to be read from the json config file, or maybe an environment variable
-# CHANGE_PATH
-#mounted_from="/home/sfsuser/Vaidehi/amos-rep/test_tree"
-
-# I don't understand how to work with this "self" in python.
-
-MOUNTED_FILES = ""
-FUSE_MOUNTED_FILES_LIST = "/tmp/Mounted_files.lst"
-MDH_CORE = "core-sfs"
-MOUNTED_FROM = "/home/sfsuser/Vaidehi/amos-rep/test_tree"
-
-
 
 class TestIntegratedTool(unittest.TestCase):
+
 
     def setUp(self):
         print("/************************************* Entered setUp *************************************/")
 
-        # Add docker stop $(docker ps -aq), docker rm $(docker ps -aq) -- may not be required now. Still keeping in comments if we have problems later and this is a resolution
-        if not os.path.isfile('docker-compose.yml'):
-            self.assertRaises(FileNotFoundError)
+        self.mounted_files = []
+        self.FUSE_MOUNTED_FILES_LIST = "/tmp/Mounted_files.lst"
+        self.MDH_CORE = "core-sfs"
+        self.MOUNTED_FROM = "/home/sfsuser/Vaidehi/amos-rep/test_tree"
+
 
         # Check if mdh core is running
         is_core_up = subprocess.Popen(["docker", "container", "inspect", "-f",
-	                              "'{{.State.Running}}'", MDH_CORE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	                              "'{{.State.Running}}'", self.MDH_CORE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = is_core_up.communicate()
         if "true" not in out.decode():
-            self.assertFalse("MDH CORE NOT RUNNING!!!")
+            print("MDH CORE NOT RUNNING!!!")
+            exit()
 
         docker_compose_up=subprocess.Popen(["docker-compose", "up"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        docker_up_out, docker_up_err = docker_compose_up.communicate()
+        if docker_up_err.decode().__contains__("error") == True:
+            print("ERROR RUNNING DOCKER-COMPOSE UP!")
+            exit()
 
         # Check if subprocess is running
         is_up=docker_compose_up.poll()
@@ -52,8 +46,8 @@ class TestIntegratedTool(unittest.TestCase):
                 ["docker", "container", "start", "synthetic-file-system"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             is_up = docker_start.poll()
             if is_up is not None:
-                self.assertFalse(
-                    "Container synthetic-file-system cannot be started!!")
+                print("Container synthetic-file-system cannot be started!!")
+                exit()
         else:
             # Added sleep because docker takes time to be set up
             time.sleep(30)
@@ -75,21 +69,22 @@ class TestIntegratedTool(unittest.TestCase):
         docker_mount_out, docker_mount_err=docker_mount.communicate()
         docker_mount_err = docker_mount_err.decode()
         if "failed" in docker_mount_err:
-             self.assertFalse("ERROR RUNNING MOUNT.SH!!!")
+             print("ERROR RUNNING MOUNT.SH!!!")
+             exit()
 
         time.sleep(5)
 
         # goes into the container and gets a list of all files under fuse_mount and writes it to a file
-        MOUNTED_FILES=os.popen(
+        self.mounted_files=os.popen(
             'docker container exec synthetic-file-system /bin/sh -c "cd ..; cd /fuse_mount; find . -type f" ').read()
-        f=open(FUSE_MOUNTED_FILES_LIST, 'w')
-        print(MOUNTED_FILES, file=f)
+        f=open(self.FUSE_MOUNTED_FILES_LIST, 'w')
+        print(self.mounted_files, file=f)
         f.close()
 
         # Throws error if file containing mounted data is empty
-        if os.stat(FUSE_MOUNTED_FILES_LIST).st_size == 0:
-            self.assertFalse("Mounted files list at " +
-                             FUSE_MOUNTED_FILES_LIST, " empty!!!")
+        if os.stat(self.FUSE_MOUNTED_FILES_LIST).st_size == 0:
+            print("Mounted files list at " + self.FUSE_MOUNTED_FILES_LIST, " empty!!!")
+            exit()
 
 
 
@@ -97,10 +92,10 @@ class TestIntegratedTool(unittest.TestCase):
     def test_dir_structure(self):
         print("/************************************* Entered test_dir_structure *************************************/")
 
-        common_dir = MOUNTED_FROM.split("/")
+        common_dir = self.MOUNTED_FROM.split("/")
 
         # Read all fuse files into an array
-        fuse_mounted_files=open(FUSE_MOUNTED_FILES_LIST, "r")
+        fuse_mounted_files=open(self.FUSE_MOUNTED_FILES_LIST, "r")
         fuse_abs_mount_lines=fuse_mounted_files.readlines()
         fuse_mounted_files.close()
 
@@ -109,7 +104,7 @@ class TestIntegratedTool(unittest.TestCase):
                             for i in fuse_abs_mount_lines]
 
         # Checks if files found at mounted_from were actually mounted on the vfs
-        for root, dirs, files in os.walk(MOUNTED_FROM):
+        for root, dirs, files in os.walk(self.MOUNTED_FROM):
             for f in files:
                 # /home/sfsuser/Vaidehi/amos-rep/test_tree/dir0/dir144/dir158/dir160/dir164/dir165/CNV-1699976-2.jpeg
                 file_path=os.path.join(root, f)
@@ -131,5 +126,4 @@ class TestIntegratedTool(unittest.TestCase):
 
         # COuld do with an assertion error, I don't know which one.
         if is_down is None:
-            self.assertFalse(
-                "************** COULD NOT SHUT DOWN DOCKER **************")
+            print("************** COULD NOT SHUT DOWN DOCKER **************")

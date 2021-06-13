c76d112ceb0f6a6311c78c9632d57a0e4d6bcc7a
sandraaocode <sandra.arbo@fau.de>
2021-06-13 18:15:57 +0200

    Finished run script

--- run-sfs.sh
+++ run-sfs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
-if [[ <get first argument> == "pytest" ]]; then
-    shift 1  # if you start the script with ./run-sfs.sh pytest -v, pytest will be shiftet away so "$1" is now "-v"
-    <insert command for pytest>
-elif [[ <insert condition for flake8> ]]; then
-    <insert command for flake8>
+if [[ "$1" == "pytest" ]]; then
+    shift 1  # if you start the script with ./run-sfs.sh pytest -v, pytest will be shifted away so "$1" is now "-v"
+    python "-m" "pytest" "tests/unit" "--cov=sfs" "$@"
+elif [[ "$1" == "flake8" ]]; then
+    shift 1
+    flake8 "sfs" "tests" "--count" "--show-source" "--max-line-length=127" "--per-file-ignores=__init__.py:F401" "--statistics" "$@"
 else
-    python3 "-m" "sfs" "$@"  # "$@" corresponds to all the arguments that were not shifted away
+    python "-m" "sfs" "$@"  # "$@" corresponds to all the arguments that were not shifted away
 fi
+
